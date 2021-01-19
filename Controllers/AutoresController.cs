7272d4000043a016ae5fc5800539e151a1ecb04b
Olga Alarcon <olgagpeam@gmail.com>
2021-01-18 20:57:03 -0600

    Autor Controller added

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -4,41 +4,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ProyectoBiblioteca.Models;
+
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ProyectoBiblioteca.Controllers
 {
+    //prueba git
     [Route("api/[controller]")]
     [ApiController]
-    public class AutoresController : ControllerBase
+    public class AutoresControllers : ControllerBase
     {
-        // GET: api/<AutoresController>
+        // GET: api/<BibliotecariosController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Autor> Get()
         {
-            return new string[] { "value1", "value2" };
+            var context = new bibliotecaContext();
+            var autores = from b in context.Autor
+                            select new Autor
+                            {
+                                NombreAutor = b.NombreAutor,
+                                ApellidoPaterno = b.ApellidoPaterno,
+                                ApellidoMaterno = b.ApellidoMaterno,
+                                FechaNacimiento = b.FechaNacimiento
+                            };
+            return autores;
         }
 
-        // GET api/<AutoresController>/5
+        // GET api/<BibliotecariosController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<AutoresController>
+        // POST api/<BibliotecariosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Autor value)
         {
+            var context = new bibliotecaContext();
+            Autor autores = new Autor
+            {
+                NombreAutor = value.NombreAutor,
+                ApellidoPaterno = value.ApellidoPaterno,
+                ApellidoMaterno = value.ApellidoMaterno,
+                FechaNacimiento = value.FechaNacimiento
+            };
+            context.Autor.Add(autores);
+            context.SaveChanges();
         }
 
-        // PUT api/<AutoresController>/5
+        // PUT api/<BibliotecariosController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<AutoresController>/5
+        // DELETE api/<BibliotecariosController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
